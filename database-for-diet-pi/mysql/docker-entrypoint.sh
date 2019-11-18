4d29384cd7e94c3a11276d97aae7722471df0cee
lhysin <lhysin@github.io>
2019-11-18 21:42:23 +0900

    change file

--- database-for-diet-pi/mysql/docker-entrypoint.sh
+++ database-for-diet-pi/mysql/docker-entrypoint.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-
 set -e
-set -x
 
-host="$1"
-shift
-cmd="$@"
+trap 'kill -2 1; wait 1' SIGTERM
 
-until mysql -u root -proot -e 'select 1'; do
-  >&2 echo "MySQL is unavailable - sleeping"
-  sleep 1
-done
+# if [ "$1" = 'mongod' ]; then
+#     #exec /sbin/tini -- -g /usr/bin/mongod
+#     exec /usr/bin/mongod "$@"
+# fi
 
-mysql -u root -proot -e "create user IF NOT EXISTS 'lhysin'@'%' identified by 'lhysin';" -e "grant all privileges on *.* to 'lhysin'@'%';" -e "update mysql.user set Host = 'localhost' where User = 'root';" -e "FLUSH PRIVILEGES;"
-
->&2 echo "Mysql is up - executing command"
-exec $cmd
+exec "$@"
