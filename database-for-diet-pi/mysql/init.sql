cbde9d2bd6c803829d8be5239b5d44b80db7be56
lhysin <lhysin@github.io>
2019-11-19 00:00:59 +0900

    change file

--- database-for-diet-pi/mysql/init.sql
+++ database-for-diet-pi/mysql/init.sql
@@ -1,3 +1,3 @@
-GRANT ALL PRIVILEGES on *.* to 'lhysin'@'%' IDENTIFIED BY 'root';
+GRANT ALL PRIVILEGES on *.* to 'lhysin'@'%' IDENTIFIED BY 'lhysin';
 UPDATE mysql.user set Host = 'localhost' where User = 'root';
 FLUSH PRIVILEGES;
